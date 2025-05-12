e8e2a5e473c56554e173c200dd4853b36701861a
Inaciocb <inaciocbdev@gmail.com>
2025-05-12 20:28:56 -0300

    landing+routebutton

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,48 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="flex justify-center items-center min-h-screen px-4">
+      <div className="max-w-md text-center">
+        <h1 className="text-4xl font-bold text-green-900 mb-8">
+          Regras do Jardim Botânico
+        </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="space-y-1 text-lg text-green-900 leading-relaxed text-left">
+          <p>• Não jogar lixo no chão.</p>
+          <p>• Não fumar.</p>
+          <p>• Não tomar bebidas alcoólicas, ou quaisquer substâncias ilícitas.</p>
+          <p>
+            • Para passear com os pets, usar sempre a guia e permanecer nos entornos da sede.
+          </p>
+          <p>• Não entrar com o carro sem autorização.</p>
+          <p>
+            • Não portar armas, nem instrumentos destinados ao corte de vegetação, captura, caça, pesca ou quaisquer outras atividades prejudiciais à fauna e flora.
+          </p>
+          <p>• Jamais abandonar animais, nem plantar em áreas abertas sem autorização.</p>
+          <p>• Jamais colher plantas, nem as flores, nem os frutos.</p>
+          <p>
+            • Jamais capturar animais silvestres, insetos, peixes, ou qualquer outro material biológico, ainda que caídos no chão.
+          </p>
+          <p>
+            • Jamais maltratar animais silvestres, nem depredar e/ou destruir patrimônio público.
+          </p>
+          <p>• Não utilizar aparelhos sonoros, a menos que utilize fones de ouvido.</p>
+          <p>• Sempre assinar o livro de visitações quando visitar o JB-UFSM.</p>
+          <p>• Respeitar as datas e horários de funcionamento.</p>
+          <p>• Respeitar os colegas e as orientações que são dadas durante a visitação.</p>
+          <p>
+            • Lembrar sempre que um dos objetivos mais importantes do JB-UFSM é a preservação das espécies.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <Link
+          href="/feed"
+          className="inline-block px-10 py-2 m-3 mb-5 bg-green-800 text-white font-bold rounded-xl hover:bg-green-700 transition"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          Feed
+        </Link>
+      </div>
+    </main>
   );
 }
